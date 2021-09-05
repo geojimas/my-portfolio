69f135e9e6b9ad7f5d12daeb15605c4de4456532
Dimitris Georgiadis <geojimas@gmail.com>
2021-09-05 22:36:00 +0300

    rmv lvl

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne:
     "Hi i'm a Jim, Junior Full Stack Developer, i have bachelor degree in computer science.",
   paragraphTwo:
-    'I like making websites and web applications , i call myself full stack developer because i like both front-end and back-end things. I mainly work with MEVN Stack (MongoDB / MySQL, ExpressJS, VueJS, NodeJS) and PHP Laravel / Lumen with MySQL and VueJS, but i also like other languages like Python and Go. If you want check out my projects on GitHub. Iam a simple person, I like to code and learn new things every day, my goal is to become a good developer with active role.',
+    'I like making websites and web applications , i call myself full stack developer because i like both front-end and back-end things. I mainly work with MEVN Stack (MongoDB / MySQL, ExpressJS, VueJS, NodeJS) and  MySQL for Database, but i also like other languages like PHP, Python and Go. If you want check out my projects on GitHub. Iam a simple person, I like to code and learn new things every day, my goal is to become a good developer with active role.',
   paragraphThree: "Also don't forget to check out my resume.",
   resume:
     'https://www.canva.com/design/DAET0C9JptA/9DUG4oEDZPfytTrr32atww/edit?category=tACZCki4tbY&tailoringShowExploreOnboarding=', // if no resume, the button will not show up
@@ -129,15 +129,15 @@ export const projectsData = [
     url: 'https://lumrest.herokuapp.com/api/movies',
     repo: 'https://github.com/geojimas/Lumen-REST-API', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'lvlcrud.png',
-    title: 'Laravel CRUD',
-    info: "It's a web app that can create, read, update, delete data in MySQL Database.",
-    info2: 'Made with PHP Laravel Framework, Bootstrap.css, Materialize.css and MySQL Database.',
-    url: 'http://lvlcrud.herokuapp.com',
-    repo: 'https://github.com/geojimas/Laravel-CRUD', // if no repo, the button will not show up
-  },
+  // {
+  //   id: nanoid(),
+  //   img: 'lvlcrud.png',
+  //   title: 'Laravel CRUD',
+  //   info: "It's a web app that can create, read, update, delete data in MySQL Database.",
+  //   info2: 'Made with PHP Laravel Framework, Bootstrap.css, Materialize.css and MySQL Database.',
+  //   url: 'http://lvlcrud.herokuapp.com',
+  //   repo: 'https://github.com/geojimas/Laravel-CRUD', // if no repo, the button will not show up
+  // },
 ];
 
 // // CONTACT DATA
